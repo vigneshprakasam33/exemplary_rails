8df8fd05d262ee7dcb3129a3edbdcbb1dcb21e5a
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2015-06-14 14:34:20 +0530

    exemplary html5 template integrated into layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exemplary::Application.config.secret_key_base = '2dab9f804d8784ac4356da4f7b47d48669873b8e61eebb83ac3a0c90b785654556104ae601cd388897452034f9986d45db66f618f42a9e9d5f1bc5aab85cb283'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Exemplary::Application.config.session_store :cookie_store, key: '_exemplary_session'
